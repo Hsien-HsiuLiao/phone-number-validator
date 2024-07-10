e9592bc048a04205d825dd3d07d36c31b6499c3f
Hsien <hsien-hsiu.liao@yandex.com>
2024-07-09 18:14:30 -0700

    update handleSubmit for telephoneCheck

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import { useState } from 'react';
 function App() {
 
   function telephoneCheck(str) {
-
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp
+    //check for (
     var re = /(\d)/g;
     var newArr = [];
     newArr = str.split('');
+    console.log(newArr);
+
     var str2 = '';
     str2 = newArr[0];
     str2 = str2.replace(re, 'x');
@@ -17,16 +20,20 @@ function App() {
     if (newArr[0] !== '(' && str2 !== 'x') {
       return false;
     }
+
+    //    check for - (dash)
     re = /[-]/g;
     var newstr = str.replace(re, '');  //var test = new RegExp(str,/\d/);
     if (newstr.length === 10) {
       return true;
     }
+    //check for 
     re = /\s+/g;
     newstr = newstr.replace(re, '');
     if (newstr.length === 10) {
       return true;
     }
+    //check for leading 1
     re = /[1]/g;
     newstr = newstr.replace(re, '');
     if (newstr.length === 10) {
@@ -52,13 +59,13 @@ function App() {
 
 
 
-  telephoneCheck("555-555-5555");
+  /* telephoneCheck("555-555-5555");
   //telephoneCheck("(6505552368)");
   telephoneCheck("1 555)555-5555");
   telephoneCheck("1 (555) 555-5555");
   telephoneCheck("-1 (757) 622-7382");
   telephoneCheck("1 555-555-5555");
-
+ */
 
   //https://react.dev/learn/reacting-to-input-with-state#step-2-determine-what-triggers-those-state-changes
   const [answer, setAnswer] = useState('');
@@ -66,15 +73,23 @@ function App() {
   const [status, setStatus] = useState('typing');
 
   if (status === 'success') {
-    return <h1>That's right!</h1>
+    return <h1>Valid phone number</h1>
   }
 
   async function handleSubmit(e) {
     e.preventDefault();
     setStatus('submitting');
     try {
-      await submitForm(answer);
-      setStatus('success');
+      //      await submitForm(answer);
+      telephoneCheck(answer);
+      console.log('telephoneCheck(answer)', telephoneCheck(answer));
+      if (telephoneCheck(answer) === true) {
+
+        setStatus('success');
+      }
+      if (telephoneCheck(answer) === false) {
+        setError(false);
+      }
     } catch (err) {
       setStatus('typing');
       setError(err);
@@ -102,18 +117,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           This app will take an input and check if it is a valid phone number
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
 
       <h2>phone number validator</h2>
